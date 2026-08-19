5d6e6cf3fb0b788696662ed5cd3a9ffab94d11b2
yahisml22-del <yahisml22@gmail.com>
2026-08-19 13:23:27 +0100

    Update sw.js assets to include app icons

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ const CACHE_NAME = 'medprogress-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
